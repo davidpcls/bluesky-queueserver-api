e8f77008398ff3db6f7f67ef7911336ded6d7af5
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2026-08-19 10:05:59 -0400

    TST: update deprecated parameter in start-re-manager call

--- src/bluesky_queueserver_api/tests/test_comm.py
+++ src/bluesky_queueserver_api/tests/test_comm.py
@@ -102,7 +102,7 @@ def test_ReManagerComm_ZMQ_02(monkeypatch, re_manager_cmd):  # noqa: F811
     set_qserver_zmq_address(monkeypatch, zmq_server_address=zmq_server_addr)
     # Configure and start RE Manager
     monkeypatch.setenv("QSERVER_ZMQ_PRIVATE_KEY", private_key)
-    re_manager_cmd(["--zmq-addr", zmq_manager_addr])
+    re_manager_cmd(["--zmq-control-addr", zmq_manager_addr])
 
     RM = ReManagerComm_ZMQ_Threads(zmq_control_addr=zmq_server_addr, zmq_public_key=public_key)
     result = RM.send_request(method="queue_item_add", params=params)
